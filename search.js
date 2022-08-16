e7321ecaacb3e235c096a24e1fc18a0c1a7bef56
saber nikfar <nikfar.saber@gmail.com>
2022-08-17 03:35:38 +0430

    add delete and search ability in css and js on task-list page

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 const textBoxSearch = document.querySelector(".search-textbox");
 const backOrCloseImage = document.querySelector(".back-to-home-img");
-
+// const deletTasks = document.querySelectorAll(".trash-img");
 function searchButtonClick() {
   if (textBoxSearch.style.visibility !== "visible") {
     textBoxSearch.style.visibility = "visible";
@@ -12,6 +12,7 @@ document
   .querySelector(".back-to-home-button")
   .addEventListener("click", function () {
     if (textBoxSearch.style.visibility == "visible") {
+      textBoxSearch.value = "";
       textBoxSearch.style.visibility = "hidden";
       backOrCloseImage.src = "assets/arrow-left.png";
       document;
@@ -28,20 +29,79 @@ function updateTasks() {
     Health: "Personal-tasks",
     Other: "Other-tasks",
   };
+  console.log(document.querySelector(".search-textbox").value);
   if (localStorage.getItem("todolistsource") !== null) {
     objectSource = JSON.parse(localStorage.getItem("todolistsource"));
     for (i = 0; i < objectSource.length; ++i) {
+      if (
+        objectSource[i].taskName.search(
+          document.querySelector(".search-textbox").value
+        ) == -1
+      )
+        continue;
+      let newDiv = document.createElement("div");
+      newDiv.id = `divId${i}`;
+      newDiv.className = "chart-task";
+      let newImg = document.createElement("input");
+      newImg.id = `TaskId${i}`;
+      newImg.type = "Image";
+      newImg.src = "assets/edit.png";
+      newImg.className = "edit-img";
+      newImg.classList.add("reset-color");
       let newButton = document.createElement("button");
+      newButton.id = `buttonTaskId${i}`;
       newButton.className = classTypeArray[objectSource[i].taskType];
+      newButton.classList.add("button-task");
+      newButton.classList.add("reset-color");
       let newH3 = document.createElement("h3");
       newH3.textContent = objectSource[i].taskName;
       let newP = document.createElement("p");
       newP.textContent = objectSource[i].taskDescription;
+      let newTrashImg = document.createElement("input");
+      newTrashImg.type = "Image";
+      newTrashImg.src = "assets/trash.png";
+      newTrashImg.classList.add("trash-img");
+      newTrashImg.id = `trashId${i}`;
       newButton.appendChild(newH3);
       newButton.appendChild(newP);
-      document.querySelector("main").appendChild(newButton);
+      newDiv.appendChild(newImg);
+      // newImg.attributes();
+      newImg.setAttribute("onfocus", "unFocusEditImg(this.id)");
+      newDiv.appendChild(newButton);
+      newDiv.appendChild(newTrashImg);
+      newTrashImg.setAttribute("onclick", "deletTask(this.id)");
+      document.querySelector("main").appendChild(newDiv);
     }
   }
 }
 
 updateTasks();
+
+document.body.addEventListener("mousedown", fn, true);
+
+function fn() {
+  const allForResetColor = document.querySelectorAll(".reset-color");
+  for (const resetColorElement of allForResetColor) {
+    resetColorElement.style.backgroundColor = "#fff";
+    resetColorElement.style.color = "#000";
+  }
+}
+
+function unFocusEditImg(id) {
+  document.querySelector(`#button${id}`).style.backgroundColor = "#fbd2e3";
+  document.querySelector(`#button${id}`).style.color = "#ee2375";
+  document.querySelector(`#${id}`).style.backgroundColor = "#fbd2e3";
+}
+
+function deletTask(id) {
+  const objectSource = JSON.parse(localStorage.getItem("todolistsource"));
+  objectSource.splice(+id.replace("trashId", ""), 1);
+  localStorage.setItem("todolistsource", JSON.stringify(objectSource));
+  document.querySelector("main").innerHTML = "";
+  updateTasks();
+}
+
+function searchTextChange() {
+  document.querySelector("main").innerHTML = "";
+  updateTasks();
+}
